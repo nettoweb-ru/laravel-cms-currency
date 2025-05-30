c3851e31a1b14be6984ce5bda828e60e94a930da
Alexander <contact@nettoweb.ru>
2025-05-30 06:37:10 +0300

    Korean support

---
+++ lang/ko/main.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    "create_currency" => "새로운 화폐",
+    "currency" => "화폐",
+    "exchange_rate" => "환율",
+    "exchange_rates" => "환율",
+    "exchange_provider" => "데이터 소스",
+    "list_currency" => "화폐",
+];
